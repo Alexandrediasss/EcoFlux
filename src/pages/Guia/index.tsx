850fcfaa4eedc9c4fdb3f11dbf8b55ab8d82cf82
MariaLuiza007-Teo <ot.marialuiza@gmail.com>
2026-04-20 12:07:05 -0300

    Atualizando imagem

--- src/pages/Guia/index.tsx
+++ src/pages/Guia/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 // Importa o componente que contém todo o seu código
-import GuiaBiodigestor from "./components/conceito_biodigestor"; 
+import GuiaBiodigestor from "./components/inicio.tsx"; 
 
 const GuiaPage: React.FC = () => {
     return (
